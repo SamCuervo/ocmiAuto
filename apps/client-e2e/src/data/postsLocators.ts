1b6c9cf113e5e424b09b469a843cd5edc96ebc89
Sam Cuervo <bscg20000619@gmail.com>
2024-11-09 18:05:31 -0500

    fix: correct validation logic for posts titles in DB check

--- apps/client-e2e/src/data/postsLocators.ts
+++ apps/client-e2e/src/data/postsLocators.ts
@@ -4,7 +4,7 @@ export const postsLocators ={
     titleInputLocator:'input[placeholder="Enter your post title"]',
     ContentInputLocator:'textarea[placeholder="Write your post content here..."]',
     CreatePostBtnLocator:'button[type="submit"]',
-    titlePostTxtLocator:'h3.text-2xl.font-semibold.leading-none.tracking-tight',
+    titlePostTxtLocator:'div[role="article"] h3.text-2xl',
     contentPostTxtLocator:'p[class="whitespace-pre-wrap"]',
     userNamePostTxtLocator:'div.flex.items-center.gap-2 svg.lucide-user + span',
     favoriteBookPostTxtLocator: 'div.text-sm.mt-1 span:nth-of-type(2)',
